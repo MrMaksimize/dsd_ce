cb0fad3736cb569ca3024dd937c74f6c82ece49f
Maksim Pecherskiy <max@maksimize.com>
2017-04-11 13:22:04 -0700

    Formatting.

--- main.go
+++ main.go
@@ -1,64 +1,64 @@
 package main
 
-import ("io/ioutil"
-	"github.com/scoutred/opendsd"
-	"log"
+import (
+	"bytes"
 	"flag"
 	"fmt"
-	"os"
-	"bytes"
-	"github.com/gocarina/gocsv"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/gocarina/gocsv"
+	"github.com/scoutred/opendsd"
+	"io/ioutil"
+	"log"
+	"os"
 	"reflect"
-    )
+)
 
 func check(e error) {
-    if e != nil {
-	log.Fatal(e)
-        panic(e)
-    }
+	if e != nil {
+		log.Fatal(e)
+		panic(e)
+	}
 }
 
 type CeCase struct { // Our example struct, you can use "-" to ignore a field
-    ID      string `csv:"case_id"`
-    CaseSource string `csv:"case_source"`
+	ID         string `csv:"case_id"`
+	CaseSource string `csv:"case_source"`
 }
-
 
 func main() {
 
-    filePath := flag.String("filePath", "", "xml file path of code enforcement data")
-flag.Usage = func() {
-	fmt.Fprintln(os.Stderr, "usage: dsd_ce [options]")
-	flag.PrintDefaults()
-    }
-    flag.Parse()
+	filePath := flag.String("filePath", "", "xml file path of code enforcement data")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "usage: dsd_ce [options]")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
 
-    dat, err := ioutil.ReadFile(*filePath)
-    check(err)
+	dat, err := ioutil.ReadFile(*filePath)
+	check(err)
 
-    buf := bytes.NewBufferString(string(dat))
-    cases, err := opendsd.DecodeCodeEnforcementCases(buf)
-    check(err)
-    //fmt.Printf("%+v\n", cases)
-    ce_cases := cases.Cases
-    spew.Dump(ce_cases[0])
-    fmt.Println(reflect.TypeOf(ce_cases))
-    ce_cases2 := CeCase(ce_cases)
-    csvContent, err := gocsv.MarshalString(&ce_cases2)
-    check(err)
-    fmt.Print(string(csvContent))
-    //fmt.Println("fpath: ", *filePath)
+	buf := bytes.NewBufferString(string(dat))
+	cases, err := opendsd.DecodeCodeEnforcementCases(buf)
+	check(err)
+	//fmt.Printf("%+v\n", cases)
+	ce_cases := cases.Cases
+	spew.Dump(ce_cases[0])
+	fmt.Println(reflect.TypeOf(ce_cases))
+	ce_cases2 := CeCase(ce_cases)
+	csvContent, err := gocsv.MarshalString(&ce_cases2)
+	check(err)
+	fmt.Print(string(csvContent))
+	//fmt.Println("fpath: ", *filePath)
 
-    /*project, err := client.ProjectByID(*projectID)
-    if err != nil {
-	log.Fatal(err)
-    }
-    //log.Printf("project %v", project)
+	/*project, err := client.ProjectByID(*projectID)
+	    if err != nil {
+		log.Fatal(err)
+	    }
+	    //log.Printf("project %v", project)
 
-    b, err := json.Marshal(project)
-    if err != nil {
-	log.Println("error:", err)
-    }
-    os.Stdout.Write(b)*/
+	    b, err := json.Marshal(project)
+	    if err != nil {
+		log.Println("error:", err)
+	    }
+	    os.Stdout.Write(b)*/
 }
